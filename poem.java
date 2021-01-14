88ae881e6e5a5c14dd821abafb54f101f46c81f8
yoel <67129044+yoel0@users.noreply.github.com>
2021-01-14 06:51:43 -0800

    [add] My first Java Poem

---
+++ poem.java
@@ -0,0 +1,26 @@
+class poem {
+  public static void main(String[] args) {
+    System.out.println("You should've known we were coming.\n"
+                        + "We came up from nothing, Swimming in fire.\n"
+                        + "Bootcamp Grads, my Boyz and Girlz.\n"
+                        + "Thinking we bluffing.\n"
+                        + "CODE destruction.\n"
+                        + "Way that we hop on an IDE, destroy it without an eruption.\n"
+                        + "You should've known WE were coming.\n"
+                        + "Bug combustion.\n"
+                        + "Way that we growing and slaying these bugs.\n"
+                        + "We HUNTING.\n"
+                        + "I against I, We fighting a battle and war with ourselves.\n"
+                        + "We don't see no one else.\n"
+                        + "Showing love ALWAYS in scope.\n"
+                        + "Soul laced HIGHER order function.\n"
+                        + "Our minds ain't really for sale.\n"
+                        + "Didn't know we were coming, now they do.\n"
+                        + "We been doing work yeah, times TWO.\n"
+                        + "Challenges? Please, Winnie D Pooh.\n"
+                        + "Crushing Mountains, Haiku.\n"
+                        + "We lit, Kung-Fu.\n"
+                        + "We blessed, ACHOO.\n"
+                        + "We are Next, like .js! How about you?");
+  }
+}
